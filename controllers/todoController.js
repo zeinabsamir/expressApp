896c0e0e932c92adfc5cec9496eee42f4d2fd68d
zeinab <zeinabsamir8@gmail.com>
2018-04-25 17:59:27 +0200

    Get data from the monogdb and pass it to the view

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose');
 // connect to databases
 mongoose.connect('mongodb://test:test@ds213259.mlab.com:13259/todo');
 
-var data = [{item: 'study'}, {item: 'clean the house'}, {item: 'go out with my boyfriend'}];
+// var data = [{item: 'study'}, {item: 'clean the house'}, {item: 'go out with my boyfriend'}];
 
 // Create a schema - this is like a blueprint
 var todoSchema = new mongoose.Schema({
@@ -12,17 +12,19 @@ var todoSchema = new mongoose.Schema({
 });
 
 var Todo = mongoose.model('Todo', todoSchema);
-var itemOne = Todo({item: 'buy flowers'}).save(function(err){
-    if (err) throw err;
-    console.log('item saved');
-});
+
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 module.exports = function(app) {
 
 app.get('/todo', function(req, res){
-  res.render('todo', {todos: data});
+  // get data from mongodb and pass it to view
+  Todo.find({}, function(err, data){
+      if (err) throw err;
+      res.render('todo', {todos: data});
+  });
+
 });
 
 app.post('/todo', urlencodedParser, function(req, res){
