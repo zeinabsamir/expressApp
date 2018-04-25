bbe45cea72c8aa38cb9082afed48092c8a494974
zeinab <zeinabsamir8@gmail.com>
2018-04-25 17:51:39 +0200

    Creat a database and connect to it and define a schema for data

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,7 +1,21 @@
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+// connect to databases
+mongoose.connect('mongodb://test:test@ds213259.mlab.com:13259/todo');
 
 var data = [{item: 'study'}, {item: 'clean the house'}, {item: 'go out with my boyfriend'}];
 
+// Create a schema - this is like a blueprint
+var todoSchema = new mongoose.Schema({
+    item: String
+});
+
+var Todo = mongoose.model('Todo', todoSchema);
+var itemOne = Todo({item: 'buy flowers'}).save(function(err){
+    if (err) throw err;
+    console.log('item saved');
+});
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
