6e40b4b15337350448ba45b0867e30e32bb7e3a5
zeinab <zeinabsamir8@gmail.com>
2018-04-25 15:28:31 +0200

    Add some dummy data

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,7 +1,8 @@
+var data = [{item: 'study'}, {item: 'clean the house'}, {item: 'go out with my boyfriend'}]
 module.exports = function(app) {
 
 app.get('/todo', function(req, res){
-  res.render('todo');
+  res.render('todo', {todoes: data});
 });
 
 app.post('/todo', function(req, res){
